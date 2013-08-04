27a8b0bb0fe628d508e6a839c5f74f6f5f080143
Thomas Weißschuh <thomas@t-8ch.de>
2013-08-04 08:49:01 +0000

    move data dir to toplevel

--- ugrm/config.py
+++ ugrm/config.py
@@ -1,6 +1,6 @@
 from path import path
 
-DATADIR = str(path(__file__).dirname() / path('data') / path('usergroup'))
+DATADIR = str(path(__file__).dirname().parent / path('data') / path('usergroup'))
 
 ADMIN_EMAIL = 'ugrm-cal@t-8ch.de'
 PRODID = '-//UGRM cal//{}//'.format(ADMIN_EMAIL)
